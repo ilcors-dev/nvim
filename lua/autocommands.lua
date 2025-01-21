a5e9124118f0d7fb8fffcd07d698f1e967692b3b
ilcors-dev <corsetti.luca28@gmail.com>
2025-01-21 14:19:41 +0100

    remove existing generated files before re-generating them in .qak

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,8 +1,14 @@
 vim.api.nvim_create_autocmd("BufWritePost", {
   pattern = "*.qak",
   callback = function()
+    local current_path = vim.fn.expand "%:p:h"
+    -- remove any it.unibo.* folders
+    local command = "rm -rf " .. current_path .. "/it*"
+    print "Removing it.unibo.* folders"
+    os.execute(command)
+
     local file = vim.fn.expand "%:p" -- Full path of the current file
-    local command = "java -jar /Users/ilcors-dev/Desktop/qak/it.unibo.Qactork_1.5.6.jar " .. file
+    command = "java -jar /Users/ilcors-dev/Desktop/qak/it.unibo.Qactork_1.5.6.jar " .. file
     print("Generating Kotlin files for: " .. file)
     os.execute(command)
   end,
